bf85284868fe7f7bada3f06c8e69d34e6ce777f6
Paul Halliday <paulhalliday@pauls-imac.home>
2017-12-03 08:49:14 +0000

    fixed bad import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PdfViewerModule],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
