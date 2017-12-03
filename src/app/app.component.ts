2e10b2b325acc5a1f02fd5e8f4bdd3f31bc4376c
Paul Halliday <paulhalliday@pauls-imac.home>
2017-12-03 08:47:35 +0000

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'app';
+  output: string;
+
+  savePdf() {
+    const doc = new jsPDF();
+
+    doc.text('Hello World!', 10, 10);
+    this.output = doc.output('datauri');
+
+    const pdfObj = <HTMLObjectElement>document.getElementById('obj');
+
+    pdfObj.data = this.output;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, PdfViewerModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
